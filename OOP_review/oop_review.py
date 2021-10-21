9f05964f53f33beb7b988ab9c155733d322cad4a
Aghast365 <38799808+Aghast365@users.noreply.github.com>
2021-10-21 18:44:35 -0400

    More files added

---
+++ OOP_review/oop_review.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+dir_path = os.path.abspath(os.path.dirname(__file__))
+print("Path to directory: ", dir_path)
+
+path = os.path.join(dir_path, "data\data.csv")
+print("Path to file: ", path)
+
+import csv
+
+with open(path) as csv_file:
+    pass
