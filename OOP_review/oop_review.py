e60286a13ebcfb44a63fa3802a722826226da31d
Dan <38799808+Aghast365@users.noreply.github.com>
2021-10-21 18:47:05 -0400

    Update oop_review.py

--- OOP_review/oop_review.py
+++ OOP_review/oop_review.py
@@ -4,7 +4,7 @@ import sys
 dir_path = os.path.abspath(os.path.dirname(__file__))
 print("Path to directory: ", dir_path)
 
-path = os.path.join(dir_path, "data\data.csv")
+path = os.path.join(dir_path, "data/data.csv")
 print("Path to file: ", path)
 
 import csv
